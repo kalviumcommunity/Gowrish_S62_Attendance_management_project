6ccceba99acb5deb22174fde27efb56ec4d1650e
unknown <Gowrishankar.s@btech.christuniversity.in>
2025-07-26 12:10:45 +0530

    part6 done

--- src/com/school/Course.java
+++ src/com/school/Course.java
@@ -1,16 +1,21 @@
+
 package com.school;
 
-public class Course {
-    int courseId;    
-    String courseName;
+public class Course implements Storable {
+    private int courseId;    
+    private String courseName;
     private static int nextCourseIdCounter = 101;
 
     public Course(String courseName) {
         this.courseId = nextCourseIdCounter++;
         this.courseName = courseName;
     }
+    @Override
+    public String toDataString() {
+        return "Course," + courseId + "," + courseName;
+    }
     public int getCourseId() {
-        return courseId ;
+        return courseId;
     }
     public String getCourseName() {
         return courseName;
@@ -19,5 +24,3 @@ public class Course {
         System.out.println("Course ID: C" + this.courseId + ", Name: " + this.courseName);
     }
 }
-
-
